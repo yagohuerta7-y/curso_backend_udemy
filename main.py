64058e85bd26d98f670a3e431727d728458e8d94
Yago <yagoraulhuertaorozco@gmail.com>
2026-02-01 23:04:46 -0600

    Update FastAPI app title and add product listing endpoint (POST)

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
 
-app = FastAPI(title="My firts blog")
+app = FastAPI(title="BD del almacen")
 
+# Definimos la variable con los datos
+productos = [
+    {"id": 1, "name": "Sandia", "cantidad": 5, "precio": 25},
+    {"id": 2, "name": "Mango", "cantidad": 12, "precio": 15},
+    {"id": 3, "name": "Manzana", "cantidad": 50, "precio": 8},
+    {"id": 4, "name": "Piña", "cantidad": 8, "precio": 30},
+    {"id": 5, "name": "Platano", "cantidad": 24, "precio": 5}
+]
 
-@app.get("/home")
+@app.get("/Home", tags=["Home"])
 def home():
-    return {'message': 'Hola, este es mi primer endpoint de yago'}
+    return {'message': 'Bienvenido a la BD del almacen'}
+
+@app.post("/Productos", tags=["Productos"])
+def listar_productos():
+    return {"data": productos}
